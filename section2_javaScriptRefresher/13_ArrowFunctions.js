852efb59746f6468e71361368efb44f1d819ccd8
thexluca <thexluca@gmail.com>
2023-05-19 03:15:45 -0500

    Added notes on Arrow Functions and Destructuring

--- section2_javaScriptRefresher/13_ArrowFunctions.js
+++ section2_javaScriptRefresher/13_ArrowFunctions.js
@@ -29,4 +29,28 @@ window.onload = function() {
     const multiplyRewritten = number => number * 2;
     console.log( multiplyRewritten(2) );
 
+        // * Arrow Functions Scope 
+    // Arrow functions do NOT block off the scope of 'this'. 
+    const tahoe = {
+        mountains: ["Freel", "Rose", "Tallac", "Rubicon", "Silver"],
+        print: function(delay = 1000){
+            setTimeout(() => {
+                console.log(this.mountains.join(", "));
+            }, delay);
+        } 
+    };
+    tahoe.print(); // Freel, Rose, Tallac, Rubicon, Silver
+    // "print" property uses a function expression(blocking outside scopes). 
+    // Therefore 'this' can only point to its object, tahoe. 
+    // Inside "print", setTimeout() uses a arrow function(NOT blocking outside scope) allowing outside scope of 'this' to be its parent function scope, referring to the object, tahoe.
+    // If setTimeout() was a function expression, all scopes would be blocked, 'this' would just point to window. 
+
+    // ** Return Objects 
+    const person = (firstName, lastName) => ({
+        first: firstName,
+        last: lastName
+    });
+    console.log(person('Flad', 'Hanson'));
+    // note: To return objects, wrap the returned object with parentheses. 
+
 }
--- section2_javaScriptRefresher/18_Destructuring.js
+++ section2_javaScriptRefresher/18_Destructuring.js
@@ -2,12 +2,19 @@ window.onload = function() {
     // Destructuring
     // Easily extract array elements or object proprieties and store them in variables.
 
-    // Array destructuring. 
+    // ** Array destructuring. 
     const numbers = [1, 2, 3 ];
     [num1, ,num3] = numbers; // notice it skips num2 with leavin a blank between commas. 
     console.log(num1, num3);
 
-    // Object Destructuring
+    // .. more destructuing arrays
+    const animals = ['horse', 'mouse', 'cat'];
+    const [firstAnimal] = animals;
+    console.log(firstAnimal); // horse
+    const [, , thirdAnimal] = animals;
+    console.log(thirdAnimal); // cat
+
+    // ** Object Destructuring
     const person = {
         name : 'thexluca',
         age : 32,
@@ -19,4 +26,44 @@ window.onload = function() {
     // obj destructuring example 2 
     ({name: value1, age: value2} = person); // The parentheses ( ... ) around the assignment statement are required when using object literal destructuring assignment without a declaration.
     console.log(value2);
+
+    // .. more destructuring objects 
+    const sandwich = {
+        bread: "dutch crunch",
+        meat: "tuna",
+        cheese: "swiss",
+        topping: ['lettuce', 'tomatoes', 'mustard']
+    };
+
+    let { bread, meat } = sandwich;
+    console.log(`${bread} and ${meat}`); // dutch crunch and tuna 
+
+    // * Destructuring object passed in function arguments. 
+    // Instead of using dot notation syntax to dig into objects, we can 
+    // destructure the value we need out of an object. 
+    // ex.
+    const lordify = ( {firstname} ) => {
+        console.log(`${firstname} of Canterbury`);
+    };
+    const regularPerson = {
+        firstname: 'Bill',
+        lastname: 'Wilson'
+    };
+    lordify(regularPerson); // Bill of Canterbury
+
+    // Further, destructuring object with a nested object 
+    // ex.
+    const lordifySpouse = ( {spouse: {firstname}} ) => {
+        console.log(`${firstname} of Canterbury`);
+    };
+    const regularPersonSpouse = {
+        firstname: 'Bill',
+        lastname: 'Wilson',
+        spouse: {
+            firstname: 'Andrea',
+            lastname: 'Wilson'
+        }
+    };
+    lordifySpouse(regularPersonSpouse); // Andrea of Canterbury
+
 }
