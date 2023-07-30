8aee1e8bcaf48c59edff8e035c983f473a7e0cd8
thexluca <thexluca@gmail.com>
2023-07-29 20:44:38 -0500

    Created notes on video 37 and 38

---
+++ section3_reactBasicsAndWorkingWithComponents/37_38_Components_Declerative.js
@@ -0,0 +1,25 @@
+
+// *** 37. WHAT ARE COMPONENTS *** 
+
+// Reusable building blocks in your user interface. Components are just a 
+// combination of HTML code, CSS code for styling and JavaScript for logic. 
+// React is all bout components, you build individual components and then you 
+// tell React how to compose them together into a final user interface.
+
+// ** WHY USE COMPONENTS? ** 
+// 1. Reusability
+// Having reusable building blocks helps us avoid repetition.
+// 2. Separation of Concerns
+// Keep codebase small and manageble, every component has one clear and simple 
+// concern. This creates small pieces of code that are easy to manage and maintian. 
+
+
+// *** 38. React Code Is Written In A "Declarative Way"! *** 
+
+// ** HOW IS A COMPONENT BUILT ** 
+// React allows you to create re-usable and reactive components 
+// consisting of HTML and JavaScript (and a bit of CSS).
+
+// ** DECLERATIVE APPROACH ** 
+// Define the desired targets state(s) and let React figure out 
+// the actual JavaScript DOM instructions. 
