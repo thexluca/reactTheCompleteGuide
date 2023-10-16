2f0d2f248e4fe5332cc842528e5a10c58f6c5d5e
thexluca <thexluca@gmail.com>
2023-10-16 17:25:00 -0500

    Creted more components and added props to pass data

--- section3_reactBasicsAndWorkingWithComponents/01-starting-setup/src/App.js
+++ section3_reactBasicsAndWorkingWithComponents/01-starting-setup/src/App.js
@@ -1,6 +1,8 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
+import React from "react";
+import ReactDOM from "react-dom";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -26,28 +28,18 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem>
+      <Expenses items={expenses} />
     </div>
   );
+
+  // *** JSX alternative for code abover ***
+  //return React.createElement(
+  //  "div",
+  //  {},
+  //  React.createElement("h2", {}, "Let's get started!"),
+  //  React.createElement(Expenses, { items: expenses })
+  //);
+  // *** end JSX alternative for code abover ***
 }
 
 export default App;
